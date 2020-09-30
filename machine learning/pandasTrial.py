5a20f886ac5d3243bc7b372cbf5fb188b76377ce
Abbas Malu <60291537+abbas-malu@users.noreply.github.com>
2020-09-30 19:16:24 +0530

    MACHINE LEARNING DATA ADDING

---
+++ machine learning/pandasTrial.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import numpy as np
+import json
+
+netflix_data = pd.read_csv('archive/netflix_titles.csv')
+# print(netflix_data.columns)
+# print(netflix_data.loc[11:120,'title'])
+# print(netflix_data.release_year == 2018)
+print(netflix_data.loc[netflix_data.release_year == 2018].head())
+for year in netflix_data.release_year:
+    # print(year.unique())
+    pass
