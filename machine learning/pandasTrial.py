4095e6fd09833da49f25c88de4c73ab5006befaf
Abbas Malu <60291537+abbas-malu@users.noreply.github.com>
2020-10-01 13:04:09 +0530

    update

--- machine learning/pandasTrial.py
+++ machine learning/pandasTrial.py
@@ -7,7 +7,10 @@ netflix_data = pd.read_csv('archive/netflix_titles.csv')
 # print(netflix_data.columns)
 # print(netflix_data.loc[11:120,'title'])
 # print(netflix_data.release_year == 2018)
-print(netflix_data.loc[netflix_data.release_year == 2018].head())
+# print(netflix_data.loc[netflix_data.release_year == 2018].head())
 for year in netflix_data.release_year:
     # print(year.unique())
     pass
+# print(len(list(range(len(netflix_data.title),0,-1))))
+years = netflix_data.release_year.value_counts()
+print(years)
