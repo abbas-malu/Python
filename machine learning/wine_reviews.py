50ecbacba9ad60e88e5cb167fe8e0c46dcdd500a
Abbas Malu <60291537+abbas-malu@users.noreply.github.com>
2020-10-03 18:02:36 +0530

    Update wine_reviews.py

--- machine learning/wine_reviews.py
+++ machine learning/wine_reviews.py
@@ -8,9 +8,13 @@ reviews = pd.read_csv(reviews_path)
 ser = pd.Series(reviews.loc[(reviews.description.str.contains('tropical')) & (reviews.points>=1),'points'])
 # print(ser)
 
-def points_increase(row):
-    row.points += 1
-    return row
+# def points_increase(row):
+#     row.points += 1
+#     return row
 
-point_inc = reviews.apply(points_increase,axis='columns')
-print(point_inc.loc[:,'points'])
+# point_inc = reviews.apply(points_increase,axis='columns')
+# print(point_inc.loc[:,'points'])
+
+# country_wise_data = pd.Series(reviews.country.map(lambda cntry: reviews.loc[reviews.country == cntry,'points'].sum() ),index=reviews.country.unique().tolist())
+points_per_country = reviews.groupby('country').points.sum()
+points_per_country.sort_values(by='points', ascending=False)
